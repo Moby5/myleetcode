91ed9cc4f0f30eed4bb77d7da0360614b2380805
Moby5 <moby5@foxmail.com>
2017-05-19 09:44:00 +0800

    add a new solution

---
+++ python/isSameTree.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+http://bookshadow.com/weblog/2016/08/18/leetcode-same-tree/
+
+LeetCode 100. Same Tree
+Given two binary trees, write a function to check if they are equal or not.
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+题目大意：
+给定两棵二叉树，编写函数检查它们是否相等。
+当且仅当两棵二叉树的结构相同并且节点值也相同时，判定为相等。
+
+解题思路：
+递归（Recursion）
+"""
+
+import argparse
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        pass
+
+
+if __name__ == '__main__':
+
+    print "hi, abby, it's your show time now!"
