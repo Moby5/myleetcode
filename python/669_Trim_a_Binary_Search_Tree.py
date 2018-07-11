0b6859e3a5f631e6cfe90160d3180994b8834e6f
Moby5 <moby5@foxmail.com>
2018-07-11 10:22:39 +0800

    add 669_Trim_a_Binary_Search_Tree.py

---
+++ python/669_Trim_a_Binary_Search_Tree.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Date: 2018-07-11
+
+"""
+669. Trim a Binary Search Tree
+DescriptionHintsSubmissionsDiscussSolution
+Pick One
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+Input: 
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output: 
+    1
+      \
+       2
+Example 2:
+Input: 
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+Output: 
+      3
+     / 
+   2   
+  /
+ 1
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        elif root.val < L:
+            return self.trimBST(root.right, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+
+"""
+http://bookshadow.com/weblog/2017/09/03/leetcode-trim-a-binary-search-tree/
+
+题目大意：
+给定二叉排序树（BST），对二叉树进行修剪，保留值位于[L, R]之间的节点
+
+解题思路：
+递归（Recursion）
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        if not root: return None
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
+"""                 
+        
